f71a2d2c67389f9efe35ee5b0919bdf61bca4937
Vielder <iljagladius@gmail.com>
2023-06-30 15:34:12 +0300

    feat: added possibility to sort list by names. Also now all task list is saved in browser cache.

---
+++ src/components/SortPanel.ts
@@ -0,0 +1,26 @@
+import TaskList from "./TaskList";
+
+export default {
+    name: "SortPanel",
+    props: {
+        tasks: {
+            type: Array,
+            required: true,
+        },
+    },
+    data() {
+        return{
+            order: false
+        }
+    },
+    methods: {
+        sortTasks() {
+            if(this.order){
+                this.tasks.sort((a, b) => a.title.localeCompare(b.title));
+            } else {
+                this.tasks.sort((a, b) => b.title.localeCompare(a.title));
+            }
+            this.order = !this.order;
+        },
+    },
+}
